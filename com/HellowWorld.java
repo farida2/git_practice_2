29fcf9a7f6273f5af003b9d5e651f46f7e03325f
farida2 <fari_belh@yahoo.co.uk>
2020-03-26 23:32:24 -0400

    first commit

---
+++ com/HellowWorld.java
@@ -0,0 +1,12 @@
+package codingnomads.com;
+
+public class HellowWorld {
+	public static void main(String[] args){
+		System.out.println("Hello user! Plaese review your parameters below:");
+		for(String val : args){
+			System.out.println(val);
+ 		}
+	}
+
+}
+
